3c7ea64ac5a74da6c2a03cfe78d4b8f50c0caaf2
isaamb <isabokas@gmail.com>
2022-09-22 12:56:56 -0400

    command handler not working??

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
 
 client.once('ready', () => {
 	console.log('Ready!');
